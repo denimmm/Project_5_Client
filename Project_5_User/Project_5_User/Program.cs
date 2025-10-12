8c585e795ca1ce67259c471f116a5c3494d21aea
denimmm <dmacdougall7987@conestogac.on.ca>
2025-10-12 11:01:28 -0400

    added driverLocation endpoint

--- Project_5_User/Project_5_User/Program.cs
+++ Project_5_User/Project_5_User/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Writers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -35,6 +37,46 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
+
+
+//returns the location of the user's driver
+//ie /driverlocation?driver=12345
+app.MapGet("/driverLocation", (HttpContext context, int driverID) =>
+{
+
+    //verify the user's authentication token
+    var authHeader = context.Request.Headers["Authorization"].ToString();
+
+    //make sure string is not empty
+    if (string.IsNullOrEmpty(authHeader))
+        return Results.Unauthorized();
+
+    //send auth token to auth module for verification. return unauthorized if invalid
+
+
+    //request driver location from the navigation or driver module
+    var navigationOutput = new
+    {
+        latitude = 12.4112,
+        longitude = 14.2212
+    };
+
+
+    //return driver location
+    var location = new
+    {
+        driverID = driverID,
+        latitude = navigationOutput.latitude,
+        longitude = navigationOutput.longitude
+    };
+
+    return Results.Json(location);
+})
+.WithName("GetDriverLocation")
+.WithOpenApi()
+.RequireAuthorization();
+    
+    
 
 app.Run();
 
